06a108d5952dce9c556be635dbdef8dae6982b68
ryutokizaki <ryutokizaki326@gmail.com>
2017-06-09 11:45:47 +0900

    InitializeZERO
    Let's get started :D

---
+++ Source/MomotarouZERO/MomotarouZEROGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MomotarouZEROGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOMOTAROUZERO_API AMomotarouZEROGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
